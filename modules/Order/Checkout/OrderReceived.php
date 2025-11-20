4fa2e5d36b9ec1dea3f057b236605e96309fb921
ipontt <inti.pontt@gmail.com>
2025-11-19 22:04:01 -0300

    Refactor OrderReceived to use OrderDTO

--- modules/Order/Checkout/OrderReceived.php
+++ modules/Order/Checkout/OrderReceived.php
@@ -6,13 +6,14 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
+use Modules\Order\Contracts\OrderDTO;
 
 class OrderReceived extends Mailable
 {
     use Queueable;
 
     public function __construct(
-        public string $localizedTotal,
+        public OrderDTO $order,
     ) {}
 
     public function envelope(): Envelope
@@ -25,7 +26,7 @@ class OrderReceived extends Mailable
     public function content(): Content
     {
         return new Content(
-            htmlString: "We have received your order of $this->localizedTotal.",
+            htmlString: "We have received your order of {$this->order->localizedTotal}.",
         );
     }
 }
--- modules/Order/Tests/OrderReceivedTest.php
+++ modules/Order/Tests/OrderReceivedTest.php
@@ -3,7 +3,17 @@
 use Modules\Order\Checkout\OrderReceived;
 
 it('renders the mailable', function () {
-    $mailable = new OrderReceived('$1.95');
+    $order = new \Modules\Order\Contracts\OrderDTO(
+        id: 1,
+        total: 195,
+        localizedTotal: '$1.95',
+        url: route('order::orders.show', ['order' => 1]),
+        lines: [],
+    );
+
+    $mailable = new OrderReceived($order);
+
+    // $mailable = new OrderReceived('$1.95');
 
     $this->assertIsString($mailable->render());
 });
