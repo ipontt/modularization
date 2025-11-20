96267a79d977c9d91f03305b77ebf8ff24ae5f7a
ipontt <inti.pontt@gmail.com>
2025-11-19 21:11:59 -0300

    Restructure Product modules so it works seamlessly with PHPStorm's "directory modules as composer.json packages" module system.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,6 @@ return [
     App\Providers\AppServiceProvider::class,
     Modules\Order\Infrastructure\Providers\OrderServiceProvider::class,
     Modules\Payment\Infrastructure\Providers\PaymentServiceProvider::class,
-    Modules\Product\Providers\ProductServiceProvider::class,
+    \Modules\Product\Providers\ProductServiceProvider::class,
     Modules\Shipment\Providers\ShipmentServiceProvider::class,
 ];
--- modules/Product/src/Providers/ProductServiceProvider.php
+++ modules/Product/src/Providers/ProductServiceProvider.php
@@ -8,9 +8,9 @@ class ProductServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
-        $this->mergeConfigFrom(__DIR__.'/../Config/config.php', 'product');
-        $this->loadRoutesFrom(__DIR__.'/../routes.php');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        $this->mergeConfigFrom(__DIR__.'/../../config/config.php', 'product');
+        $this->loadRoutesFrom(__DIR__.'/../Http/routes.php');
 
         $this->app->register(EventServiceProvider::class);
     }
